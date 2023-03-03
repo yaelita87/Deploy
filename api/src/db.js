931a15774c463c06afcfd2ef0d2e87a8840d7fe8
yaelita87 <pamelayaelherrera@hotmail.com>
2023-03-03 13:51:33 -0300

    modified back

--- api/src/db.js
+++ api/src/db.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const VideogameModel = require("./models/Videogame");
+const GenderModel= require("./models/Gender");
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST2,DB_PORT, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST2}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-const basename = path.basename(__filename);
 
+VideogameModel(sequelize);
+GenderModel(sequelize);
+
+const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
